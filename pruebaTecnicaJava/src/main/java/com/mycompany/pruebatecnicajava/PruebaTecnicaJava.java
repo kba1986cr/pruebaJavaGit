35e9c4351f593c617123c40839c6056fe7bdb0bf
kba1986cr <haroldsa1986@hotmail.com>
2025-04-25 10:04:07 -0600

    Agrege el proyecto java

---
+++ pruebaTecnicaJava/src/main/java/com/mycompany/pruebatecnicajava/PruebaTecnicaJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebatecnicajava;
+
+/**
+ *
+ * @author harol
+ */
+public class PruebaTecnicaJava {
+
+    public static void main(String[] args) {
+
+    }
+}
