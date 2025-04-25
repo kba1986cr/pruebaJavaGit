cd6961f24c5592c0c5f2b44eed5492b7ad4aa1bc
kba1986cr <haroldsa1986@hotmail.com>
2025-04-25 11:28:46 -0600

    Actualizacion

--- pruebaTecnicaJava/src/main/java/com/mycompany/pruebatecnicajava/PruebaTecnicaJava.java
+++ pruebaTecnicaJava/src/main/java/com/mycompany/pruebatecnicajava/PruebaTecnicaJava.java
@@ -1,8 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.pruebatecnicajava;
+
+import java.util.Scanner;
 
 /**
  *
@@ -10,7 +11,51 @@ package com.mycompany.pruebatecnicajava;
  */
 public class PruebaTecnicaJava {
 
-    public static void main(String[] args) {
+    //Variables
+    static char asientos[][] = new char[10][10];
+    static boolean bandera = false;
+    static Scanner teclado = new Scanner(System.in);
+    static int fila, asiento;
+    static String respuesta;
+
+    // Método para llenar asientos
+    public static void llenarasientosL() {
+        for (int f = 0; f < 10; f++) {
+            for (int c = 0; c < 10; c++) {
+                asientos[f][c] = 'L';
+            }
+        }
 
     }
+
+    public static void main(String[] args) {
+        llenarasientosL();
+
+        System.out.println("Bienvenido al Sistema de Recervas");
+
+        while (bandera != true) {
+            System.out.println("Ingrese Fila y Asiento a reservar");
+            System.out.println("Ingrese a Fila (Entre 0 y 9)");
+            fila = teclado.nextInt();
+            System.out.println("Ingrese a Asiento (Entre 0 y 9)");
+            asiento = teclado.nextInt();
+
+            if (asientos[fila][asiento] == 'L') {
+                asientos[fila][asiento] = 'X';
+                System.out.println("El asiento fue reservado correctamente");
+            } else {
+                System.out.println("El asienti esta ocupado. Por favor elija otro");
+
+            }
+            System.out.println("¿Desea finalizar la reserva? S:(para Si) N:(cualquier tecla NO)");
+            respuesta = teclado.next();
+
+            if (respuesta.equalsIgnoreCase("S")) {
+                bandera = true;
+            }
+
+        }
+
+    }
+
 }
